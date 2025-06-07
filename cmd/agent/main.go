1781b933ce60360b03f1695a5a03a38bffdd6cf0
RuslanSemenikhin <semenikhinra@mail.ru>
2025-06-07 17:12:38 +0300

    iter2 first commit...

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,49 @@
 package main
 
-func main() {}
+import (
+	"github.com/RuslanSemenikhin/go-service-with-metrics.git/internal/managers"
+)
+
+var AGENT *managers.AgentManager
+
+func init() {
+	realMetricsName := []string{
+		"Alloc",
+		"BuckHashSys",
+		"Frees",
+		"GCCPUFraction",
+		"GCSys",
+		"HeapAlloc",
+		"HeapIdle",
+		"HeapInuse",
+		"HeapObjects",
+		"HeapReleased",
+		"HeapSys",
+		"LastGC",
+		"Lookups",
+		"MCacheInuse",
+		"MCacheSys",
+		"MSpanInuse",
+		"MSpanSys",
+		"Mallocs",
+		"NextGC",
+		"NumForcedGC",
+		"NumGC",
+		"OtherSys",
+		"PauseTotalNs",
+		"StackInuse",
+		"StackSys",
+		"Sys",
+		"TotalAlloc",
+		"RandomValue",
+	}
+
+	counterMetricsNames := []string{"PollCount"}
+	AGENT = managers.NewAgent().
+		WithTimeForMetricsTicker(2).
+		WithTimeForServerTicker(10).WithMetricsName(realMetricsName, counterMetricsNames)
+}
+
+func main() {
+	AGENT.Start()
+}
